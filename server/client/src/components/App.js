a8249fddceca08a8222d95e09fa47164bbeae981
Godfrey Best <gcb246@stern.nyu.edu>
2017-08-19 08:02:20 -0400

    react-router added

---
+++ server/client/src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Landing = () => <h2>Landing</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+// Browser Router expects at most one child component
+
+// Browser Router checks to see if each Route's path contains
+// a string that is in the current url path
+
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    { /* Route components treated like normal components */ }
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/surveys" component={Dashboard}/>
+                    <Route path="/surveys/new" component={SurveyNew}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
---
+++ server/client/src/reducers/index.js
@@ -0,0 +1,7 @@
+// when you import any folder it will automatically give us the index.js file
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+
+export default combineReducers({
+    auth: authReducer
+})
