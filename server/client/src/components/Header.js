39f073d92180c51e8550a23eedfd53037bd42e8b
Godfrey Best <gcb246@stern.nyu.edu>
2017-08-20 07:58:01 -0400

    materialize added

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+// do not have to have state in class based component,
+// can just use it to organize helper functions
+class Header extends React.Component {
+    render() {
+        return (
+          <nav>
+              <div className="nav-wrapper">
+                  <a href="" className="left brand-logo">
+                      Emaily-Stripe
+                  </a>
+                  <ul className="right">
+                      <li>
+                          <a href="">Login with Google</a>
+                      </li>
+                  </ul>
+              </div>
+          </nav>
+        );
+    }
+}
+
+export default Header;
